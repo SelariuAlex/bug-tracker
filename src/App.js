d0dfa2ebebfd1162fa6b5c60377e5d96e160390b
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-19 00:30:20 +0200

    searchBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { SearchBar } from "./components/layout/SearchBar";
+import { Logs } from "./components/logs/Logs";
 import "./App.css";
 
 function App() {
@@ -8,7 +10,14 @@ function App() {
     M.AutoInit();
   });
 
-  return <div className="App">bug tracker</div>;
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
 }
 
 export default App;
