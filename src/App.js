ff9e89c3b59b0aa9be19c9dbe1dfb2162e855e4d
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-25 00:05:01 +0200

    redux log actions and reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Profiler } from "react";
+import React, { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { SearchBar } from "./components/layout/SearchBar";
