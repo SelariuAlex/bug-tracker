22f455f7642acfffbc1bb6fe1a006d16227a662f
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-28 11:07:22 +0200

    update reducer

--- src/redux/reducers/logReducer.js
+++ src/redux/reducers/logReducer.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from "../types";
 
 const initialState = {
@@ -27,10 +30,27 @@ export default (state = initialState, action) => {
         logs: [...state.logs, action.payload],
         loading: false
       };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log =>
+          log.id !== action.payload.id ? action.payload : log
+        )
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
       };
     case DELETE_LOG:
       return {
