7fbfe9dee14066607ab6bfcad28716138ae9971b
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-31 16:49:19 +0200

    get techs list model

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Logs from "./components/logs/Logs";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import { AddTechModal } from "./components/techs/AddTechModal";
-import { TechListModal } from "./components/techs/TechListModal";
+import TechListModal from "./components/techs/TechListModal";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,23 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { getTechs } from "../../redux/actions/techActions";
 import TechItem from "./TechItem";
 
-export const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const TechListModal = ({ tech: { techs, loading }, getTechs }) => {
   useEffect(() => {
     getTechs();
   }, []);
-
-  const getTechs = async () => {
-    setLoading(true);
-
-    const res = await fetch("/techs");
-    const data = await res.json();
-
-    setTechs(data);
-    setLoading(false);
-  };
 
   return (
     <div id="tech-list-modal" className="modal">
@@ -25,9 +14,15 @@ export const TechListModal = () => {
         <h4>Tech List</h4>
         <ul className="collection">
           {!loading &&
+            techs !== null &&
             techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
         </ul>
       </div>
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  tech: state.tech
+});
+export default connect(mapStateToProps, { getTechs })(TechListModal);
