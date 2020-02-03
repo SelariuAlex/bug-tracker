dfd6d35f9fad2cee0ec80e007cff3a27146dd00f
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-02-03 19:12:54 +0200

    tech error reducer

--- src/redux/reducers/techReducer.js
+++ src/redux/reducers/techReducer.js
@@ -23,12 +23,20 @@ export default (state = initialState, action) => {
     case ADD_TECH:
       return {
         ...state,
-        techs: [...state.techs, action.payload]
+        techs: [...state.techs, action.payload],
+        loading: false
       };
     case SET_LOADING:
       return {
         ...state,
         loading: true
+      };
+    case TECHS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
       };
     default:
       return state;
